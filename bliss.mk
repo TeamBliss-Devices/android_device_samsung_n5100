c2d979d7e280b8f1f63a05873bcdc57e0eba2e17
Jack <jackeagle102@gmail.com>
2014-10-15 16:38:56 +0530

    Blissfy

--- bliss.mk
+++ bliss.mk
@@ -1,18 +1,18 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/beanstalk/config/gsm.mk)
+$(call inherit-product, vendor/bliss/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := n5100
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/n5100/full_n5100.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n5100
-PRODUCT_NAME := beanstalk_n5100
+PRODUCT_NAME := bliss_n5100
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N5100
 PRODUCT_MANUFACTURER := samsung
