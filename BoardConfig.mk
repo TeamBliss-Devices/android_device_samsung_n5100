2094f8a8f94c1110c3d134db8274ee31b1e34c3f
Shaun Eaton <shaunsibu@gmail.com>
2015-04-23 23:41:42 -0600

    Add Bliss build flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,3 +53,19 @@ RECOVERY_TOUCHSCREEN_SWAP_XY := true
 
 # assert
 TARGET_OTA_ASSERT_DEVICE := GT-N5100,kona3g,n5100,kona3gxx
+
+# BlissPop Config Flags
+BLISS_WIPE_CACHES := 1
+TARGET_TC_ROM := 4.8
+TARGET_TC_KERNEL := 4.8
+BLISSIFY := true
+#BLISS_O3 := true
+#BLISS_STRICT := true
+#BLISS_GRAPHITE := false
+#BLISS_KRAIT := true
+#BLISS_PIPE := true
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+
+#SaberMod
+-include vendor/bliss/config/sm.mk
